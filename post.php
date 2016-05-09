7e3377f6a550cdfe6e95bf77c393bf9929582c9e
Luqman Yasin <luqman.yasin@gmail.com>
2016-05-09 10:25:05 +0100

    Fixed url var.

--- post.php
+++ post.php
@@ -28,7 +28,7 @@ if($_POST['url']) {
   $event = [[$new_name => $_POST['url']]];
   $filename = "files.json";
   $ev = preg_replace(array('/'.preg_quote('[').'/','/'.preg_quote(']').'/'), '', json_encode($event, true));
-  print $url.'https://lyasin.me/?'.$new_name;
+  print $url.'/?'.$new_name;
     
   $handle = @fopen($filename, 'r+');
   if ($handle === null) {
